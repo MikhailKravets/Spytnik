6dd8c2f70cf6ed9c6259be3ce9a9d42a9c47b588
Mikhail Kravets <22kravets22@mail.ru>
2017-11-29 11:03:22 +0200

    Update XOR example

--- examples/xor.py
+++ examples/xor.py
@@ -4,7 +4,11 @@ import layers
 from nn import FeedForward
 
 
-nn = FeedForward().create([2, 2, 1], default=layers.Tanh)
+nn = FeedForward() # .create([2, 2, 1], default=layers.Tanh)
+
+nn += layers.Linear(2, 2)
+nn += layers.Tanh(2, 1)
+nn += layers.Linear(1, 0)
 
 s = [
     ([0, 0], [0]),
@@ -22,7 +26,7 @@ for i in range(10_000):
 print(nn)
 
 for v in s:
-    print(nn.get(v[0]))
+    print(nn.get(v[0]), end='\n\n')
 
 plot.plot(E)
 plot.show()
