1150a916438f3483e2c000caaa72e930e4bca1a0
Mikhail Kravets <22kravets22@mail.ru>
2017-11-29 23:06:05 +0200

    Playing with approximation

--- examples/approximation.py
+++ examples/approximation.py
@@ -13,15 +13,15 @@ def f(x):
 
 nn = FeedForward(momentum=0.2, learn_rate=0.05, weight_decay=0.2)  # .create([2, 2, 1], default=layers.Tanh)
 
-nn += layers.Linear(1, 7)
+nn += layers.Linear(1, 10)
 
-nn += layers.Tanh(7, 8)
-nn += layers.Tanh(8, 7)
-nn += layers.Tanh(7, 1)
+nn += layers.Tanh(10, 10)
+nn += layers.Tanh(10, 10)
+nn += layers.Tanh(10, 1)
 
 nn += layers.Linear(1, 0)
 
-x = numpy.linspace(-1.6, 2.5, 150)
+x = numpy.linspace(-2.2, 2.5, 150)
 y = f(x)
 
 error = []
