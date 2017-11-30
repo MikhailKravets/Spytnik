6137a2e4f8470afee4af4884e42d32b08f183a5a
Mikhail Kravets <22kravets22@mail.ru>
2017-11-30 20:23:04 +0200

    Playing with hard problem

--- examples/the_hard_one.py
+++ examples/the_hard_one.py
@@ -13,15 +13,15 @@ from core.estimators import cv
 
 training, validation = separate_data(from_csv("D:\\DELETE\\Дипломмо\\output.csv"), 0.15)
 
-nn = FeedForward(learn_rate=0.05, momentum=0.2, weight_decay=0.7)
+nn = FeedForward(learn_rate=0.05, momentum=0.00, weight_decay=0.6)
 
 nn += layers.Linear(6, 110)
-nn += layers.Tanh(110, 150)
-nn += layers.Tanh(150, 175)
-nn += layers.Relu(175, 175)
-nn += layers.Relu(175, 175)
-nn += layers.Relu(175, 175)
-nn += layers.Tanh(175, 50)
+nn += layers.Tanh(110, 250)
+nn += layers.Tanh(250, 275)
+nn += layers.Tanh(275, 275)
+nn += layers.Tanh(275, 275)
+nn += layers.Tanh(275, 275)
+nn += layers.Tanh(275, 50)
 nn += layers.Tanh(50, 8)
 nn += layers.Linear(8, 0)
 
@@ -29,22 +29,16 @@ error = []
 v_error = []
 print("Training starts...")
 prev = time.time()
-for i in range(20_000):
+for i in range(900):
     r = random.randint(0, len(training) - 1)
     nn.fit(*training[r])
     error.append(nn.error)
 
-    if i % 300 == 0:
+    if i % 10 == 0:
         v_error.append(cv(nn, validation))
-    if i == 7_000:
-        print("And it is still training...")
-    elif i == 13_000:
-        print("Almost half...")
-    if i == 17_000:
-        print("I see the end...")
 print(f"Training is finished! Spend time: {time.time() - prev:.2f}")
 
 plot.title("Learning error")
 plot.plot(error)
-plot.plot(v_error)
+plot.plot([i * 10 for i in range(len(v_error))], v_error)
 plot.show()
