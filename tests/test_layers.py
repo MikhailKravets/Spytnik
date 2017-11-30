bf5cefb490416e363d7cc0c93e7193a318d53123
Mikhail Kravets <22kravets22@mail.ru>
2017-11-30 12:44:32 +0200

    Fix tests

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -3,22 +3,22 @@ import layers
 
 
 class TestLayers(unittest.TestCase):
-    def sigmoid(self):
+    def test_sigmoid(self):
         l = layers.Sigmoid(3, 3)
         self.assertEqual(l.a(0), 0.5)
         self.assertEqual(l.der(0), 0.25)
 
-    def tanh(self):
+    def test_tanh(self):
         l = layers.Tanh(3, 3)
         self.assertEqual(l.a(0), 0)
         self.assertEqual(l.der(0), 1)
 
-    def linear(self):
+    def test_linear(self):
         l = layers.Linear(3, 3)
         self.assertEqual(l.a(3), 3)
         self.assertEqual(l.der(23), 1)
 
-    def relu(self):
+    def test_relu(self):
         l = layers.Relu(3, 3)
         self.assertEqual(l.a(3), 3)
         self.assertEqual(l.a(-3), 0)
