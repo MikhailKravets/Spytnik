d749cbdaa729507c679f47a2689ee964c465f053
Mikhail Kravets <22kravets22@mail.ru>
2017-12-05 18:46:26 +0200

    Working on hard one

--- examples/the_hard_one.py
+++ examples/the_hard_one.py
@@ -6,15 +6,22 @@ import random
 import matplotlib.pyplot as plot
 import time
 
+import numpy
+
 import layers
 from core import FeedForward, Ensemble, from_csv, separate_data, noise
 from core.estimators import cv
 
 
+def normal(arr):
+    s = numpy.sum(numpy.abs(arr))
+    return numpy.abs(arr) / s
+
+
 training, validation = separate_data(from_csv("D:\\DELETE\\Дипломмо\\output.csv"), 0.15)
 
-# noise(training, from_range=(0, 2), axis=0)
-# noise(training, from_range=(-0.05, 0.05), axis=1)
+noise(training, from_range=(0, 2), axis=0)
+noise(training, from_range=(-0.05, 0.05), axis=1)
 
 ff1 = FeedForward(learn_rate=0.05, momentum=0.2, weight_decay=0.5)
 ff1 += layers.Linear(6, 23)
@@ -42,11 +49,17 @@ ff3 += layers.Linear(8, 0)
 
 ensemble = Ensemble(ff1, ff2, ff3)
 
+test = (
+    [10, 12, 11, 0, 0, 1],
+    [10, 12, 0, 0, 0, 1],
+    [25, 10, 0, 0, 6, 11],
+)
+
 error = []
 v_error = []
 print("Training starts...")
 prev = time.time()
-for i in range(1500):
+for i in range(1200):
     r = random.randint(0, len(training) - 1)
     ensemble.fit(*training[r])
     error.append(ensemble.error)
@@ -54,6 +67,9 @@ for i in range(1500):
     if i % 10 == 0:
         v_error.append(cv(ensemble, validation))
 print(f"Training is finished! Spend time: {time.time() - prev:.2f}")
+
+for t in test:
+    print(f"{t} -> {normal(ensemble.get(t)) * 50}")
 
 plot.title("Learning error")
 plot.plot(error)
